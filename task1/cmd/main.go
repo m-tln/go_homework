548a61486cf29af691bba06eb78f90dd4032b70b
tul.matvey <tul.matvey@gmail.com>
2024-09-28 20:12:38 +0300

    Finished

--- task1/cmd/main.go
+++ task1/cmd/main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"fmt"
 	. "task1/storage"
+	"time"
+
+	"github.com/google/uuid"
 )
 
+func StringID() string {
+	return uuid.New().String()
+}
+
 func main() {
+	books := []string{"The Whale", "1984", "Frankenstein"}
 	var library ILibrary = &LibraryMap{}
-	library.AddBook("The Whale")
+	library.SetIDfunc(StringID)
+
+	for _, book := range books {
+		library.AddBook(book)
+	}
+
+	fmt.Println(library.SearchByName("The Whale"))
+
+	library.SetIDfunc(time.UTC.String)
+	fmt.Println(library.SearchByName("1984"))
+
+	library = &LibrarySlice{}
+	library.SetIDfunc(StringID)
+	library.AddBook("The Great Gatsby")
+	library.AddBook("To Kill a Mockingbird")
+
+	fmt.Println(library.SearchByName("The Great Gatsby"))
 }
--- task1/storage/library.go
+++ task1/storage/library.go
@@ -1,23 +1,23 @@
 package storage
 
 type Book struct {
-	name string
+	Name string
 }
 
 type ILibrary interface {
 	IStorage
 	SearchByName(name string) (Book, bool)
 	AddBook(name string)
+	SetIDfunc(NewID func() string)
 }
 
 type LibraryMap struct {
 	StorageMap
 	GetID map[string]string
-	NewID func() string
 }
 
 func (l *LibraryMap) AddBook(name string) {
-	b := Book{name: name}
+	b := Book{Name: name}
 	key := l.Add(b)
 	if len(l.GetID) == 0 {
 		l.GetID = make(map[string]string)
@@ -30,14 +30,17 @@ func (l *LibraryMap) SearchByName(name string) (Book, bool) {
 	return book.(Book), ok
 }
 
+func (l *LibraryMap) SetIDfunc(NewID func() string) {
+	l.StorageMap.SetIDfunc(NewID)
+}
+
 type LibrarySlice struct {
 	StorageSlice
 	GetID map[string]string
-	NewID func() string
 }
 
 func (l *LibrarySlice) AddBook(name string) {
-	b := Book{name: name}
+	b := Book{Name: name}
 	key := l.Add(b)
 	if len(l.GetID) == 0 {
 		l.GetID = make(map[string]string)
@@ -48,4 +51,8 @@ func (l *LibrarySlice) AddBook(name string) {
 func (l *LibrarySlice) SearchByName(name string) (Book, bool) {
 	book, ok := l.SearchByID(l.GetID[name])
 	return book.(Book), ok
+}
+
+func (l *LibrarySlice) SetIDfunc(NewID func() string) {
+	l.StorageSlice.SetIDfunc(NewID)
 }
